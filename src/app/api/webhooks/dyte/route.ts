b871d654d350e4343f5b2197e5243cc928a22f4c
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-07-29 22:49:12 -0500

    handle recording storage

--- src/app/api/webhooks/dyte/route.ts
+++ src/app/api/webhooks/dyte/route.ts
@@ -1,12 +1,30 @@
+import { createClient } from "@supabase/supabase-js";
 import { NextRequest, NextResponse } from "next/server";
 
 export const runtime = "edge";
+const supabase = createClient(
+  "https://<supabase-project-id>.supabase.co",
+  "<supabase-service-key>"
+);
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
   switch (body.event) {
     case "recording.statusUpdate": {
-      console.log("Recording status updated", body);
+      const { status, downloadUrl, outputFileName } = body.recording;
+      if (status === "UPLOADED") {
+        const getRecordingResponse = await fetch(downloadUrl, {
+          method: "GET",
+        });
+        const blob = await getRecordingResponse.blob();
+        const file = new File([blob], "recording.mp4", { type: "video/mp4" });
+        const { error } = await supabase.storage
+          .from("recordings")
+          .upload(outputFileName, file);
+        if (!error) {
+          console.log("Recording uploaded to Supabase Storage");
+        }
+      }
       break;
     }
     default:
