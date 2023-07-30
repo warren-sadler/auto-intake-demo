e1c8d7e316008556177bf9ed1b3dda52598952b1
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-07-29 23:01:45 -0500

    prefer service role key

--- src/app/api/webhooks/dyte/route.ts
+++ src/app/api/webhooks/dyte/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from "next/server";
 export const runtime = "edge";
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.SUPABASE_PUBLIC_ROLE_KEY!
 );
 
 export async function POST(req: NextRequest) {
