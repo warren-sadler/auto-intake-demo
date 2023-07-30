23c101642c965c3562b78e38abc07b12f46b732c
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2023-07-29 22:52:06 -0500

    remove placeholders

--- src/app/api/webhooks/dyte/route.ts
+++ src/app/api/webhooks/dyte/route.ts
@@ -3,8 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const runtime = "edge";
 const supabase = createClient(
-  "https://<supabase-project-id>.supabase.co",
-  "<supabase-service-key>"
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
 export async function POST(req: NextRequest) {
